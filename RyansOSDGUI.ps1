90f4d0101bd6163eb35e9e5452047f584492c75d
rthannigan <131395768+rthannigan@users.noreply.github.com>
2023-04-20 15:26:03 -0500

    Add files via upload

---
+++ RyansOSDGUI.ps1
@@ -0,0 +1,24 @@
+﻿Write-Host  -ForegroundColor Yellow "Starting Brooks' Custom OSDCloud ..."
+cls
+Write-Host "=================== Ryan Hannigan =====================" -ForegroundColor Yellow
+Write-Host "==================== LAS IT OSD =======================" -ForegroundColor Yellow
+Write-Host "========= University of Illinois at Chicago ===========" -ForegroundColor Yellow
+Write-Host "==================== Main Menu ========================" -ForegroundColor Yellow
+Write-Host "3: I'll select it myself"-ForegroundColor Yellow
+Write-Host "4: Exit`n"-ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
+    '3' { Start-OSDCloud	} 
+    '4' { Exit		}
+}
+
+wpeutil reboot
