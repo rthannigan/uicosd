cb7e01423691121ac31a7db5ee7ba919188fef1f
rthannigan <131395768+rthannigan@users.noreply.github.com>
2023-04-26 08:26:13 -0500

    Add files via upload

---
+++ OSDCloudGUIISO.ps1
@@ -0,0 +1,7 @@
+﻿Install-Module OSD -Force
+Import-Module OSD -Force
+New-OSDCloudTemplate
+New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud3
+Edit-OSDCloudWinPE -WorkspacePath C:\OSDCloud3 -Wallpaper C:\Users\hannigan\Desktop\DaTeam.jpg -CloudDriver Dell,HP,IntelNet,LenovoDock,USB,VMware,Wifi -WebPSScript 
+
+New-OSDCloudISO -WorkspacePath C:\OSDCloud3
