f697444d63cd7f89977b21acc81c5b6fc2e7542c
rthannigan <131395768+rthannigan@users.noreply.github.com>
2023-04-26 08:25:55 -0500

    Add files via upload

---
+++ RyansOSDCloud.ps1
@@ -0,0 +1,26 @@
+﻿Write-Host -ForegroundColor Yellow -BackgroundColor Red "EAS Tech. Solutions: Windows Deployment process using OSDCloud."
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "=================== Ryan Hannigan =====================" -ForegroundColor Yellow
+Write-Host "==================== LAS IT OSD =======================" -ForegroundColor Yellow
+Write-Host "========== University of Illinois at Chicago ==========" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win10 22H2 | English | Enterprise"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win11 22H2 | English | Enterprise" -ForegroundColor Yellow
+Write-Host "3: I'll select it myself"-ForegroundColor Yellow
+Write-Host "4: Exit`n"-ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 22H1 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 22H2 -OSEdition Enterprise -ZTI } 
+    '3' { Start-OSDCloud	} 
+    '4' { Exit		}
+}
+
+wpeutil reboot
